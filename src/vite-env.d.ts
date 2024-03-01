a15a9c410e0125f714f7e2f0859860b085786003
Erick Christian <erickchristian48@gmail.com>
2024-03-01 10:21:00 +0700

    refactor: simplify env declaration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
-/// <reference types="vite/client" />
+/// <reference types="vite-node/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SLACK_WORKSPACE: string;
+  readonly VITE_SLACK_EMAIL: string;
+  readonly VITE_SLACK_PASSWORD: string;
+  readonly VITE_SPOTIFY_CLIENT_ID: string;
+  readonly VITE_SPOTIFY_CLIENT_SECRET: string;
+  readonly VITE_PORT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
